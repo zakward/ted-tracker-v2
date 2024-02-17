0225b9a1e43394e405c74fddfc16d4e52550f7a6
Zak Ward <zakward85@gmail.com>
2024-02-17 15:27:04 -0600

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,40 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {useNavigate, Link} from "react-router-dom"
 import { UserContext } from '../context/UserContext';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 function Navbar() {
     const {logout, token, user} = useContext(UserContext)
     const navigate = useNavigate()
+    const [isOpen, setIsOpen] = useState(false)
+
+    function toggleMenu(){
+        setIsOpen(!isOpen)
+
+    }
+
+    function closeLogout(){
+        setIsOpen(!isOpen)
+        logout()
+    }
     return ( 
         <nav>
-        <h3>Welcome {user.username}</h3>           
-        <Link to ="/form" className = "nav-item"><button>Add New Ted</button>   </Link>  
-        <Link to ="/tedList" className = "nav-item"><button>Saved List</button></Link> 
-       {token && <button id = "logout-btn"onClick = {logout} className = "nav-item">Log Out</button>}
-       {token && <ArrowBackIosNewIcon className="nav-item navigation" fontSize= "large" onClick = {()=> navigate(-1)}/>}
+           {isOpen ?  <CloseIcon fontSize='large'className = "menu-icon" onClick = {toggleMenu}/> :
+            <MenuIcon fontSize='large' className = "menu-icon" onClick = {toggleMenu}/>
+    }
+        <h3>Welcome {user.username}</h3> 
+        {token && <ArrowBackIosNewIcon className="nav-item navigation" fontSize= "large" onClick = {()=> navigate(-1)}/>}
 
+        {isOpen &&  
+        <>
+        <div className = {`${"menu"} ${isOpen && "active"}`}>
+        <Link to ="/form" className = "nav-item" onClick={toggleMenu}><button>Add New Ted</button>   </Link>  
+        <Link to ="/tedList" className = "nav-item" onClick={toggleMenu}><button>Saved List</button></Link> 
+       {token && <button id = "logout-btn"onClick = {closeLogout} className = "nav-item">Log Out</button>}
+        </div>
+        </>        
+        }
         </nav>
      );
 }
